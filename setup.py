6001d4f06ea2cec9933caf21d3e7576566c7a6bd
micecchi <micecchi@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-11-25 09:41:52 +0000

    initial import

---
+++ setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+This module contains the tool of collective.blogstarentry
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = '1.0.0'
+
+tests_require=['zope.testing']
+
+setup(name='collective.blogstarentry',
+      version=version,
+      description="A simple object to use for blog entries in blog.star",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        'Framework :: Plone',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        ],
+      keywords='',
+      author='RedTurtle Technology',
+      author_email='sviluppoplone@redturtle.net',
+      url='http://svn.plone.org/svn/plone/plone.example',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', ],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['setuptools',
+                        ],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+      test_suite = 'collective.blogstarentry.tests.test_docs.test_suite',
+      entry_points="""
+      # -*- entry_points -*- 
+      """,
+      )
